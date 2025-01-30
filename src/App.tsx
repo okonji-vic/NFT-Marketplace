9151c54746fd9ec51cf4f077a29d072551a58b0a
Okonji Emeka <65029583+okonji-vic@users.noreply.github.com>
2025-01-30 17:01:50 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -14,63 +14,29 @@ import FolderSection from "./FolderSection";
 import ScrollGallery from "./ScrollGallery";
 import Footer from "./Footer";
 
-// const App: React.FC = () => {
-
-//   return (
-//     // <Router>
-//     <div>
-
-//       <Navbar />
-//       <HeroSection />
-//       
-//         <BiddingList />
-//         {/* <Routes> */}
-//           {/* <Route path="/" element={<BiddingList />} /> */}
-//           {/* <Route path="/product/:id" element={<ProductDetail />} /> */}
-//         {/* </Routes> */}
-//       <TextImageSection />
-//       <TripleSection />
-//       <EmojiSection />
-//       <PopularAuctions />
-//       <SectionWithIconContainer />
-//       <FolderSection />
-//       <ScrollGallery />
-//       <Footer />
-//      
-//       </div>
-//     // </Router>
-//   );
-// };
-
-// export default App;
 
 
 const App: React.FC = () => {
   return (
     <Router>
       <div>
-        <Navbar /> {/* Always render Navbar */}
+        <Navbar /> 
         
-        {/* Routes are used to conditionally render the content */}
         <Routes>
-          {/* Home route will render BiddingList and other sections */}
           <Route path="/" element={<HomePage />} />
-          
-          {/* Product detail route will render only ProductDetail */}
           <Route path="/product/:id" element={<ProductDetail />} />
         </Routes>
 
-        <Footer /> {/* Always render Footer */}
+        <Footer /> 
       </div>
     </Router>
   );
 };
 
-// HomePage component renders all sections except when on the product detail page
 const HomePage: React.FC = () => {
   return (
     <div>
-      {/* Only render these sections when on the home page */}
+      
       <HeroSection />
       <BiddingList />
       <TextImageSection />
